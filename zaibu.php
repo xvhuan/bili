6bb81aa05d1eee8d4058c9ccfac45f3801a9ae96
xvhuan <50939413+xvhuan@users.noreply.github.com>
2020-07-03 23:27:04 +0800

    Update zaibu.php

--- zaibu.php
+++ zaibu.php
@@ -4,11 +4,12 @@ fwrite($a,"0");
 sleep(20);
 $b=fopen("jiance.txt","w+");
 fwrite($b,"1");
-postcurl("https://fastmessage.cn/bili/jiankong.php");
+postcurl("https://你的域名/jiankong.php");
 function postcurl($url){
 $curl = curl_init();//初始化curl
 curl_setopt($curl, CURLOPT_URL,$url);//访问的url
 curl_exec($curl);//执行curl;
 curl_close($curl); // 关闭CURL会话
 }
+//自动启动程序
 ?>
